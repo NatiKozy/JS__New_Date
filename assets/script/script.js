6d27f65c62fc8576f0e316f90e736c74e2365321
Nataly Kozyreva <eve30@yandex.ru>
2023-06-01 17:05:47 +0300

    add function set avatar

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,11 +1,10 @@
-
 function showData(name, imageSrc, editComments, date){
     let nameInput = document.createElement('div');
     nameInput.textContent = name;
 
     let image = document.createElement('img');
     image.classList.add('img__size');
-    image.src = imageSrc;
+    image.src = setAvatar(imageSrc);
 
     let commentsDiv = document.createElement('div');
     commentsDiv.textContent = editComments;
@@ -14,22 +13,16 @@ function showData(name, imageSrc, editComments, date){
     chatDate.classList.add('chat__date');
     chatDate.textContent = date;
 
-
     let nameContainer = document.querySelector('.service__chat');
     nameContainer.append(nameInput);
     nameContainer.append(image);
     nameContainer.append(commentsDiv);
     nameContainer.append(chatDate);
-
 }
 
-
-// - если пользователь не ввел ссылку на аватар, то должна появляться стандартная аватарка. Стандартных аватаров должно быть больше пяти, они должны подставляться в рандомном порядке.
-
-
 function newDate(){
-    let date = new Date();
-    return date;
+    let commentDate = new Date();
+    return commentDate;
 }
 
 const noName = document.getElementById('show-no');
@@ -49,8 +42,29 @@ function checkSpam(str){
     return str.replace(editComments, '***');
 }
 
+let avatar = document.getElementById('avatar').value;
 
-const input = document.getElementById('avatar');
+function setAvatar(avatar){
+    const defaultAvatar = [
+    "./assets/images/ava1.jpg",
+    "./assets/images/ava2.jpg",
+    "./assets/images/ava3.jpg",
+    "./assets/images/ava4.jpg",
+    "./assets/images/ava5.jpg",
+    "./assets/images/ava6.png",
+    "./assets/images/ava7.jpg",
+    "./assets/images/ava8.jpg",
+    "./assets/images/ava9.png",
+    "./assets/images/ava10.jpg"
+];
+
+    if (avatar === ""){
+       return defaultAvatar[Math.floor(Math.random() * defaultAvatar.length)];
+    } else {
+        return avatar;
+    }
+}
+
 const button = document.getElementById('button');
 button.addEventListener('click', () =>{
     let formattedName = correctName(document.getElementById('name').value);
@@ -65,11 +79,30 @@ button.addEventListener('click', () =>{
     let userDate = newDate();
 
     showData(formattedName, input, userComments , userDate);
-
 });
 
+// Напишите функцию `formatDate(date)`, форматирующую `date` по следующему принципу: 
 
+// - если спустя `date` прошло менее 1 секунды, вывести `"прямо сейчас"` ;
+// - в противном случае, если с `date` прошло меньше 1 минуты, вывести `"n сек. назад"`;
+// - в противном случае, если меньше часа, вывести `"m мин. назад"`;
+// - в противном случае, полная дата в формате `"DD.MM.YY HH:mm"`. А именно: `"день.месяц.год часы:минуты"`, всё в виде двух цифр, т.е. `31.12.16 10:00`.
 
+// Например:
+
+// function formatDate(date){
+//     let date = new Date();
+//     if(){
+//         console.log('прямо сейчас')
+//     }
+
+//     console.log(date);
+//     return date;
+// }
+// console.log(formatDate(new Date(new Date - 1)));
+// onsole.log(formatDate(new Date(new Date - 30 * 1000)))
+// console.log(formatDate(new Date(new Date - 5 * 60 * 1000)));
+// console.log(formatDate(new Date(new Date - 86400 * 4 * 1000)));
 
 
 
